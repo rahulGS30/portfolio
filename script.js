b3e1217610a96073076d7eff7388e8479766f568
Rahul G S <rsv30112002@gmail.com>
2024-09-18 21:25:17 +0530

    updated

--- script.js
+++ script.js
@@ -1,38 +1,6 @@
-// Typewriter Effect
-
-const texts = [
-    "DEVELOPER",
-    "DESIGNER",
-    "YOUTUBER"
-]
-
-let speed  =100;
-const textElements = document.querySelector(".typewriter-text");
-
-let textIndex = 0;
-let charcterIndex = 0;
-
-function typeWriter(){
-    if (charcterIndex < texts[textIndex].length){
-        textElements.innerHTML += texts[textIndex].charAt(charcterIndex);
-        charcterIndex++;
-        setTimeout(typeWriter, speed); 
-    }
-    else{
-        setTimeout(eraseText, 1000)
-    }
-}
-
-function eraseText(){
-    if(textElements.innerHTML.length > 0){
-        textElements.innerHTML = textElements.innerHTML.slice(0,-1);
-        setTimeout(eraseText, 50)
-    }
-    else{
-        textIndex = (textIndex + 1) % texts.length;
-        charcterIndex = 0;
-        setTimeout(typeWriter, 500)
-    }
-}
-
-window.onload = typeWriter
+var typed = new Typed(".typrwriter-text",{
+    strings : ["Drone Engineer","UI/UX Designer","Developer"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    looped : false
+})
